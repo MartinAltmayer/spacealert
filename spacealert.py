132bcf4ba7fdca13a19777c8c0b7c8144f61e115
Martin Altmayer <altmayer@mathematik.uni-kl.de>
2012-11-11 16:07:28 +0100

    Improved time assignment.

--- spacealert.py
+++ spacealert.py
@@ -62,13 +62,14 @@ class Event:
         
         
 class Alert(Event):
-    def __init__(self, time, turn, type, zone, unconfirmed=False):
+    def __init__(self, time, turn, type, zone, unconfirmed=False, ambush=False):
         super().__init__(time)
         assert (zone is None) == type.internal
         self.turn = turn
         self.type = type
         self.zone = zone
         self.unconfirmed = False
+        self.ambush = ambush
         
     def __repr__(self):
         return "{}{}{}{}{}".format(self.timeCode, 'AL' if not self.unconfirmed else 'UR', self.turn, self.type.code, self.zone.code if self.zone is not None else '')
@@ -201,6 +202,7 @@ class InvalidMissionError(ValueError):
 
     
 class MissionGenerator:
+    # Length of phases in seconds
     PHASE1_DISTRIBUTION = {
             200:     1,
             205:     3,
@@ -245,32 +247,53 @@ class MissionGenerator:
             (2,0,2,1): 3,
             (1,1,2,1): 1
         }
-        
+      
+    # Min/max threat points per phase (normal=1, serious=2)
     MIN_TP_PER_PHASE = 3
     MAX_TP_PER_PHASE = 5
-    THREAT_PART = (0.55, 0.65)
     
-    FIRST_THREAT_DISTRIBUTION = (
-        {10: 1},
-        {
-        5:  2,
-        10: 3,
-        15: 2,
-        20: 1}
-    )
+    # Distribution of the time of the first threat in a phase
+    FIRST_THREAT_DISTRIBUTION = {
+        1: {10: 1},
+        2: {
+            5:  2,
+            10: 3,
+            15: 3,
+            20: 2
+        }
+    }
     
+    # Restrict some threat types to specific turns
+    RESTRICT_TURNS = {
+        T_INTERNAL: range(2,8),
+        T_SERIOUS_INTERNAL: range(3,7)
+    }
+    
+    # Probability that an ambush happens in a phase (an ambush is a late-coming threat)
     # Remember that there are two phases in which an ambush may happen.
     # Real ambush probability is thus 1-(1-x)^2
-    AMBUSH_PROBABILITY = 0.25
+    AMBUSH_PROBABILITY = {1: 0.25, 2: 0.25}
+    # Ambushs appear basically at phase.start + phase.length*AMBUSH_PHASE_PART
+    # To make this more random they are shifted by an amount drawn from AMBUSH_MOVE_DISTRIBUTION
     AMBUSH_PHASE_PART = 0.77
     AMBUSH_MOVE_DISTRIBUTION = {
-        -10: 1,
         -5:  2,
-        0:   4,
-        5:   2,
+        0:   5,
+        5:   3,
         10:  1 
     }
  
+    # Percentage which determines the part at the beginning of a phase where threats will appear (except ambush)
+    THREAT_PART = {1: 0.65, 2: 0.65}
+    
+    # To shift the distribution of times to the beginning of the phase, this number of additional times is drawn
+    # in a phase. Then the smallest times are taken. Thus the times are basically drawn from a beta distribution.
+    SURPLUS_TIMES = {1:2, 2:0}
+    
+    # Minimal distance of threats to each other and to the phase end in seconds.
+    # Must be bigger than the time necessary to announce a threat
+    THREAT_DISTANCE = 25
+    
     def makeMission(self):
         self.state = {}
         self.mission = Mission()
@@ -294,12 +317,11 @@ class MissionGenerator:
             length = draw(dist)
             self.mission.addPhase(Phase(i, start, length))
             start += length
-
+    
     def makeThreats(self):
         # First compute the number of threats of different types that will appear
         if 'threatCounts' not in self.state:
             self.state['threatCounts'] = draw(self.THREAT_DISTRIBUTION)
-            print("ThreatCounts: {}".format(self.state['threatCounts']))
         threatCounts = self.state['threatCounts']
         threatTypes = []
         for i,tt in enumerate(THREAT_TYPES):
@@ -314,57 +336,72 @@ class MissionGenerator:
             for tt in threatTypes:
                 ttForPhase[p].append(tt)
                 p = 1 if p==2 else 2
-            if all(self.MIN_TP_PER_PHASE <= sum(tt.points for tt in tts) <= self.MAX_TP_PER_PHASE for tts in ttForPhase.values()):
-                break # found a valid assignment
+            valid = True
+            if any(not (self.MIN_TP_PER_PHASE <= sum(tt.points for tt in tts) <= self.MAX_TP_PER_PHASE) for tts in ttForPhase.values()):
+                valid = False
+            elif any(T_INTERNAL in tts and T_SERIOUS_INTERNAL in tts for tts in ttForPhase.values()):
+                valid = False
+            if valid:
+                break
             iterations += 1
             if iterations >= 100:
-                raise RuntimeError("Cannot generate a mission")
+                raise RuntimeError("Cannot assign threats to phases")
             
-        # Now choose times, turns and zones
+        # Now choose turns, times and zones
         alerts = []
         lastZone = None
         ambush = {}
-        for phase, turns in zip(self.mission.phases[:2], ([1,2,3,4],[5,6,7,8])):
-            ambush[phase.number] = random.random() < self.AMBUSH_PROBABILITY
-            chosenTurns = sorted(random.sample(turns, len(ttForPhase[phase.number])))
-            if ambush[phase.number]:
-                chosenTurns[-1] = turns[-1] # either 4 or 8
+        for phase, possibleTurns in zip(self.mission.phases[:2], ([1,2,3,4],[5,6,7,8])):
+            tts = ttForPhase[phase.number]
+            iterations = 0
+            while True:
+                ambush = random.random() < self.AMBUSH_PROBABILITY[phase.number]
+                turns = sorted(random.sample(possibleTurns, len(tts)))
+                if ambush:
+                    turns[-1] = possibleTurns[-1] # either 4 or 8
+                random.shuffle(tts)
+                valid = True
+                if any(tt in self.RESTRICT_TURNS and turn not in self.RESTRICT_TURNS[tt] for tt,turn in zip(tts,turns)):
+                    valid = False
+                elif any(tts[i].internal and tts[i-1].internal for i in range(1,len(tts))):
+                    valid = False # Two successive internal threats 
+                elif ambush and phase.number == 1 and len(tts) == 2:
+                    valid = False # Do not make ambushs in phase 1 when there is only 1 threat before (=> little to do for 2 minutes)
+                if valid:
+                    break
+                iterations += 1
+                if iterations >= 100:
+                    print("Cannot find a valid turn assignment for {}".format(phase))
+                    print("Threat counts: {}".format(threatCounts))
+                    print("Ambush: {}".format(ambush))
+                    print("Threat-types: {}".format(tts))
+                    raise RuntimeError()
+                    
             # First threat time is more or less fixed
-            chosenTimes = [phase.start + draw(self.FIRST_THREAT_DISTRIBUTION[phase.number-1])]
-            flexibleTimeCount = len(ttForPhase[phase.number]) - 1 - int(ambush[phase.number])
-            earliestPossible = chosenTimes[0]+15
-            latestPossible = phase.start + phase.length * self.THREAT_PART[phase.number-1]
-            chosenTimes.extend(sorted([round5(earliestPossible + random.random() * (latestPossible-earliestPossible))
+            times = [phase.start + draw(self.FIRST_THREAT_DISTRIBUTION[phase.number])]
+            flexibleTimeCount = len(tts) - 1 - int(ambush) + self.SURPLUS_TIMES[phase.number]
+            earliestPossible = times[0]+15
+            latestPossible = phase.start + phase.length * self.THREAT_PART[phase.number]
+            times.extend(sorted([round5(earliestPossible + random.random() * (latestPossible-earliestPossible))
                                         for i in range(flexibleTimeCount)]))
-            if ambush[phase.number]:
+            times = times[:len(tts)-int(ambush)]
+            if ambush:
                 ambushTime = round5(phase.start + phase.length * self.AMBUSH_PHASE_PART)
                 ambushTime += draw(self.AMBUSH_MOVE_DISTRIBUTION)
                 #print("Ambush in {} at {}".format(phase, ambushTime))
-                chosenTimes.append(ambushTime)
-            assert len(chosenTimes) == len(ttForPhase[phase.number])
-            shiftTimes(chosenTimes, 15, phase.end-25)
+                times.append(ambushTime)
+            shiftTimes(times, self.THREAT_DISTANCE, phase.end-self.THREAT_DISTANCE)
             
-            for time, turn, threatType in zip(chosenTimes, chosenTurns, ttForPhase[phase.number]):
+            for time, turn, threatType in zip(times, turns, tts):
                 if threatType.internal:
                     zone = None
                 else:
                     zone = random.choice([zone for zone in ZONES if zone != lastZone])
                     lastZone = zone
                 alerts.append(Alert(time, turn, threatType, zone))
-                            
-        # Check whether the threat assignment is valid
-        if (alerts[0].internal or alerts[0].serious) and alerts[0].turn == 1:
-            raise InvalidMissionError("Internal or serious threat in turn 1")
-        if (alerts[-1].internal or alerts[-1].serious) and alerts[-1].turn == 8:
-            raise InvalidMissionError("Internal or serious threat in turn 8")
-        if any(alert.type is T_SERIOUS_INTERNAL and alert.turn in [1,2,7,8] for alert in alerts):
-            raise InvalidMissionError("Serious internal threat outside turns 3-6")
-        if (alerts[0].internal and alerts[1].internal):
-            raise InvalidMissionError("Two internal threats at the beginning")
-        if len([alert for alert in alerts if alert.internal and alert.turn <= 4]) == 0 \
-                and len([alert for alert in alerts if alert.internal and alert.turn >= 5]) >= 2:
-            raise InvalidMissionError("There are more than two internal threats and all of them are in phase 2.")
-        
+            if ambush:
+                alerts[-1].ambush = True
+                
         self.mission.addEvents(alerts)
 
     
@@ -393,19 +430,40 @@ def shiftTimes(times, distance, max=None):
     
 def testAlertTimes(number):
     generator = MissionGenerator()
-    missions = [generator.makeMission() for i in range(number)]
-    times = collections.defaultdict(list)
-    for i in range(number):
+    turnTimes = collections.defaultdict(list)
+    ithTimes = {1: collections.defaultdict(list), 2: collections.defaultdict(list)}
+    ambush = {1: 0, 2: 0}
+    for x in range(number):
         mission = generator.makeMission()
+        iDict = {1: 1, 2: 1}
         for event in mission.events:
             if isinstance(event, Alert):
-                times[event.turn].append(event.time - event.phase.start)
-    means = {turn: sum(values)/len(values) for turn, values in times.items()}
+                turnTimes[event.turn].append(event.time - event.phase.start)
+                i = iDict[event.phase.number]
+                ithTimes[event.phase.number][i].append(event.time - event.phase.start)
+                iDict[event.phase.number] += 1
+                if event.ambush:
+                    ambush[event.phase.number] += 1
+    means = {turn: sum(values)/len(values) for turn, values in turnTimes.items()}
+    print("----------------------")
+    print("Threats in turns")
     for i in range(1,9):
         if i in means:
             print("{}: {}".format(i,means[i]))
         
-testAlertTimes(50)           
+    for p in 1,2:
+        print("----------------------")
+        print("i-th Threat in Phase {}".format(p))
+        for i in range(1,5):
+            if i in ithTimes[p]:
+                print("{}: {}".format(i, sum(ithTimes[p][i]) / len(ithTimes[p][i])))
+                            
+    print("----------------------")
+    print("Ambush in Phase 1: {:.2f}".format(ambush[1]/number))
+    print("Ambush in Phase 2: {:.2f}".format(ambush[2]/number))
+    
+    
+testAlertTimes(1000)          
 #generator = MissionGenerator()
 #mission = generator.makeMission()
 #mission.printLog()
